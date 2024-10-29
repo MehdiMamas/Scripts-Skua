6895648bdf713b0635db3cf08d7db24bd7d25e21
Tato <47118168+wtffidy@users.noreply.github.com>
2024-10-28 20:38:01 -0400

    Chore: CLeanup

--- ShadowsOfWar/UltraSpeakerMergePreReqs.cs
+++ ShadowsOfWar/UltraSpeakerMergePreReqs.cs
@@ -66,7 +66,6 @@ tags: ultra speaker merge, ultra malgor merge, rgow, goddess of war
 //cs_include Scripts/Story/Lair.cs
 
 using Skua.Core.Interfaces;
-using Skua.Core.Models.Items;
 // using Skua.Core.Options;
 
 public class UltraSpeakerMergePreReqs
@@ -90,48 +89,6 @@ public class UltraSpeakerMergePreReqs
     public Awescended Awescended = new();
     public CoreHollowbornPaladin CHBP = new();
     public MalgorsArmorSet MalgorsArmorSet = new();
-
-    #region not needed anymore jsut get goddesofwar
-    // public string OptionsStorage = "UltraSpeakerPreReqs";
-    // public bool DontPreconfigure = true;
-    // public List<IOption> Options = new()
-    // {
-    //     // Options for Armors
-    //     new Option<bool>("GetGoddessOfWar", "Get Goddess Of War armor[REQ]", "to get Goddess Of War armor or not.", true),
-    //     new Option<bool>("GetAscendedBladeOfAwe", "Get Ascended Blade of Awe armor", "to get Ascended Blade of Awe armor or not.", false),
-    //     // ...
-    //     // Options for Weapons
-    //     new Option<bool>("GetWarBladeOfCourage", "Get War Blade of Courage[REQ]", "to get War Blade of Courage or not.", true),
-    //     new Option<bool>("GetWarBladesOfCourage", "Get War Blades of Courage", "to get War Blades of Courage or not.", false),
-    //     new Option<bool>("GetWarBladeOfPower", "Get War Blade of Power[REQ]", "to get War Blade of Power or not.", true),
-    //     new Option<bool>("GetWarBladesOfPower", "Get War Blades of Power", "to get War Blades of Power or not.", false),
-    //     new Option<bool>("GetWarBladeOfSpeed", "Get War Blade of Speed[REQ]", "to get War Blade of Speed or not.", true),
-    //     new Option<bool>("GetWarBladesOfSpeed", "Get War Blades of Speed", "to get War Blades of Speed or not.", false),
-    //     new Option<bool>("GetWarBladeOfStrength", "Get War Blade of Strength[REQ]", "to get War Blade of Strength or not.", true),
-    //     new Option<bool>("GetWarBladesOfStrength", "Get War Blades of Strength", "to get War Blades of Strength or not.", false),
-    //     new Option<bool>("GetWarBladeOfWisdom", "Get War Blade of Wisdom[REQ]", "to get War Blade of Wisdom or not.", true),
-    //     new Option<bool>("GetWarBladesOfWisdom", "Get War Blades of Wisdom", "to get War Blades of Wisdom or not.", false),
-    //     // ...
-    //     // Options for Gauntlets
-    //     new Option<bool>("GetGoddessOfWarGauntlet", "Get Goddess of War Gauntlet", "to get Goddess of War Gauntlet or not.", false),
-    //     new Option<bool>("GetGoddessOfWarGauntlets", "Get Goddess of War Gauntlets", "to get Goddess of War Gauntlets or not.", false),
-    //     // ...
-    //     // Options for Back Items
-    //     new Option<bool>("GetGoddessOfWarPrestigeCloak", "Get Goddess Of War Prestige Cloak[REQ]", "to get Goddess Of War Prestige Cloak or not.", true),
-    //     new Option<bool>("GetMalgorsHalfWing", "Get Malgor's Half-Wing", "to get Malgor's Half-Wing or not.", false),
-    //     // ...
-    //     // Options for Helmets
-    //     new Option<bool>("GetDragonMalgorVisage", "Get Dragon Malgor Visage", "to get Dragon Malgor Visage or not.", false),
-    //     new Option<bool>("GetGoddessOfWarHelm", "Get Goddess Of War Helm", "to get Goddess Of War Helm or not.", false),
-    //     // ...
-    //     // Options for Ground Runes
-    //     new Option<bool>("GetGoddessOfWarBattlefield", "Get Goddess Of War Battlefield Rune", "to get Goddess Of War Battlefield Rune or not.", false),
-    //     // ...
-
-    //     CoreBots.Instance.SkipOptions,
-    // };
-
-    #endregion not needed anymore jsut get goddesofwar
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -373,38 +330,12 @@ public class UltraSpeakerMergePreReqs
             #endregion GoddessofWar rewrote
 
             #region RGRoW item Check
-            // Core.Logger("Checking items before buying Goddess Of War");
-
-            // // Define required items and their quantities
-            // var requiredItems = new Dictionary<int, int> { { 55901, 3 }, { 43712, 50 }, { 65841, 1 }, { 72921, 1 }, { 76482, 1 } };
-
-            // // Check if all required items are owned
-            // var allItemsOwned = requiredItems.All(item =>
-            // {
-            //     // Find the item in the inventory
-            //     ItemBase? inventoryItem = Bot.Inventory.Items.Find(x => x.ID == item.Key);
-
-            //     // Log the inventory status of the item
-            //     var isItemOwned = Core.CheckInventory(inventoryItem?.Name, item.Value);
-
-            //     // Log the inventory status
-            //     Core.Logger(!isItemOwned
-            //         ? $"Missing {inventoryItem!.Name}, {item.Value - Bot.Inventory.GetQuantity(inventoryItem!.Name)}"
-            //         : $"{inventoryItem!.Name} owned {Bot.Inventory.GetQuantity(inventoryItem!.Name) / item.Value}");
-
-            //     // Return whether the item is owned in the required quantity
-            //     return isItemOwned;
-            // });
 
             SOWM.Acquiescence(AcquiescenceCount);
             SOWM.ElementalCore(ElementalCoreCount);
 
-            // Core.Logger(allItemsOwned ? "All items owned. Attempting to buy Goddess Of War." : "Cannot buy Goddess Of War. Missing items");
-            // if (!allItemsOwned)
-            //     // Log the overall status based on item ownership
-            //     Core.Logger($"Malgor Insignia Needed: {InsigniasCount - Bot.Inventory.GetQuantity("Malgor Insignia")}");
-
             // If all required items are owned, proceed to buy the specified item
+            // Goddess Of War Prestige Cloak
             Core.BuyItem("ultraspeaker", 2248, 72921, shopItemID: 11443); //remove the else here <<
 
         }
@@ -414,133 +345,4 @@ public class UltraSpeakerMergePreReqs
         // #endregion Radiant Goddess of War quest
 
 
-    }
-
-    #region Commented out
-
-
-    // Check Default Options
-    // CheckAndSetDefaultOptions();
-
-
-    // #region WarBladeOfCourage
-    // // Weapons
-    // if (Bot.Config!.Get<bool>("GetWarBladeOfCourage"))
-    // {
-    //     if (!Core.CheckInventory("War Blade of Courage", toInv: false))
-    //     {
-    //         Core.Logger("Getting prerequisites for 'War Blade of Courage'...");
-    //         // Prerequisites for acquiring "War Blade of Courage"
-    //         if (Core.CheckInventory("Goddess Of War Blades", toInv: false) || Core.CheckInventory("War Blades of Courage", toInv: false))
-    //             Core.Logger("Buyback War Blade of Courage smh");
-    //         else
-    //         {
-    //             BLOD.BrilliantAura(50);
-    //             BLOD.BlindingAura(1);
-    //             Core.Logger("adding 10 to the Acquiescence Count");
-    //             AcquiescenceCount += 10;
-    //         }
-    //     }
-    //     if (!Core.CheckInventory("War Blades of Courage", toInv: false))
-    //     {
-    //         Core.Logger("adding 3 to the ElementalCore count");
-    //         ElementalCoreCount += 3;
-    //     }
-    // }
-    // #endregion WarBladeOfCourage
-
-    // Goddess of War Gauntlets
-    // if (Bot.Config!.Get<bool>("GetGoddessOfWarGauntlet"))
-    // {
-    //     if (!Core.CheckInventory("Goddess of War Gauntlet", toInv: false))
-    //     {
-    //         Core.Logger("Getting prerequisites for 'Goddess of War Gauntlet'...");
-    //         // Prerequisites for acquiring "Goddess of War Gauntlet"
-    //         if (Core.CheckInventory("Goddess of War Gauntlets", toInv: false))
-    //             Core.Logger("Buyback Goddess of War Gauntlet smh");
-    //         else
-    //             WFE.WarfuryEmblemFarm(50);
-    //     }
-    //     if (!Core.CheckInventory("Goddess of War Gauntlets", toInv: false))
-    //     {
-    //         Core.Logger("adding 3 to the ElementalCore count");
-    //         ElementalCoreCount += 3;
-    //     }
-    // }
-
-    // // Helmets
-    // if (Bot.Config!.Get<bool>("GetDragonMalgorVisage") || Bot.Config!.Get<bool>("GetGoddessOfWarHelm"))
-    // {
-    //     Core.Logger("Getting prerequisites for Helmets...");
-    //     if (!Core.CheckInventory("Dragon Malgor Visage"))
-    //     {
-    //         Core.Logger("adding 3 to the ElementalCore count");
-    //         ElementalCoreCount += 3;
-    //     }
-    //     if (!Core.CheckInventory("Goddess Of War Helm"))
-    //     {
-    //         Core.Logger("adding 3 to the ElementalCore count");
-    //         ElementalCoreCount += 3;
-    //     }
-    // }
-
-    // // Ground Runes
-    // if (Bot.Config!.Get<bool>("GetGoddessOfWarBattlefield"))
-    // {
-    //     Core.Logger("Getting prerequisites for Ground Runes...");
-    //     if (!Core.CheckInventory("Goddess Of War Battlefield"))
-    //     {
-    //         Core.Logger("adding 15 to the ElementalCore count");
-    //         ElementalCoreCount += 15;
-    //     }
-    // }
-
-    //split here
-
-    // // Check and set options that are turned off by default
-    // void CheckAndSetDefaultOptions()
-    // {
-    //     if (!Bot.Config!.Get<bool>("GetGoddessOfWar"))
-    //     {
-    //         Core.Logger("Get Goddess Of War armor[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetGoddessOfWar", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetWarBladeOfCourage"))
-    //     {
-    //         Core.Logger("Get War Blade of Courage[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetWarBladeOfCourage", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetWarBladeOfPower"))
-    //     {
-    //         Core.Logger("Get War Blade of Power[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetWarBladeOfPower", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetWarBladeOfSpeed"))
-    //     {
-    //         Core.Logger("Get War Blade of Speed[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetWarBladeOfSpeed", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetWarBladeOfStrength"))
-    //     {
-    //         Core.Logger("Get War Blade of Strength[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetWarBladeOfStrength", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetWarBladeOfWisdom"))
-    //     {
-    //         Core.Logger("Get War Blade of Wisdom[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetWarBladeOfWisdom", true);
-    //     }
-
-    //     if (!Bot.Config!.Get<bool>("GetGoddessOfWarPrestigeCloak"))
-    //     {
-    //         Core.Logger("Get Goddess Of War Prestige Cloak[REQ] is turned off. Turning it on to continue.");
-    //         Bot.Config!.Set("GetGoddessOfWarPrestigeCloak", true);
-    //     }
-    // }
-    #endregion Commented out
-}
+    }}
