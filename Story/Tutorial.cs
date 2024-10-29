687a3cebd45f1c78fb99a331e69cd436607c695d
Tato <47118168+wtffidy@users.noreply.github.com>
2024-10-29 19:30:12 -0400

    Update Tutorial.cs

--- Story/Tutorial.cs
+++ Story/Tutorial.cs
@@ -26,31 +26,25 @@ public class Tutorial
         if (Core.HasAchievement(31))
             return;
 
-        Core.Logger("Doing `Tutorial Badges` Required for fresh accounts to leave oaklore (this may take a moment to start.. we dont know why.)\n" +
-        "(by bot i mean.. obviously u can do this manualy)");
-
         string[] achievements = new[]
-        {
-        "Combat", "Interact", "Quest", "Skill", "Shop",
-        "Enhance", "Rest", "World", "Emotes", "Travel"
-        };
+            {
+            "Combat", "Interact", "Quest", "Skill", "Shop",
+            "Enhance", "Rest", "World", "Emotes", "Travel"
+            };
 
-        Core.Join("oaklore-999999", "r1", "Left");
+        Core.Logger("Doing `Tutorial Badges` to look a bit more \"~legit~\".");
 
-        // Ensure player is in the correct cell
-        while (!Bot.ShouldExit && Bot.Player.Cell != "r1")
-        {
-            Core.Sleep();
-            Core.Jump("r1", "Left");
-        }
+        Core.Join("oaklore");
 
         // Iterate through achievements and set them
         for (int i = 0; i < achievements.Length; i++)
         {
             string achievement = achievements[i];
-            Core.Logger($"Achievement - {achievement}");
+            if (Core.HasAchievement(22 + i))
+                Core.Logger($"Achievement: {achievement}, Status: {(Core.HasAchievement(22 + i) ? "✅" : "❌")}");
             Core.SetAchievement(22 + i);
-            Core.Sleep();
+            Bot.Wait.ForActionCooldown(GameActions.DoIA);
+            Core.Sleep(1500);
         }
     }
 
