9939a7613cfcc43045142978c4c18e0c9906712c
Tato <47118168+wtffidy@users.noreply.github.com>
2024-12-10 03:26:21 -0500

    Update HuntressMerge.cs

--- Seasonal/Frostvale/HuntressMerge.cs
+++ Seasonal/Frostvale/HuntressMerge.cs
@@ -7,6 +7,7 @@ tags: huntress-merge, seasonal, frostvale
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/Glacera.cs
 //cs_include Scripts/Seasonal/Frostvale/Frostvale.cs
 
 using Skua.Core.Interfaces;
