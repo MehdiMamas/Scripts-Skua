422b3a3c10ae24500c2460745f8dfd1b78c4a72d
Tato <47118168+wtffidy@users.noreply.github.com>
2024-12-15 12:44:10 -0500

    Update DawnsanctumMerge.cs

--- Hollowborn/MergeShops/DawnsanctumMerge.cs
+++ Hollowborn/MergeShops/DawnsanctumMerge.cs
@@ -132,7 +132,7 @@ public class DawnsanctumMerge
                 case "Vindicator Beast Tamer":
                 case "Vindicator Assassin":
                     VindicatorCrest(20);
-                    Adv.BuyItem("dawnsanctum", 2474, req.Name);
+                    Adv.BuyItem("neotower", 2474, req.Name);
                     break;
 
                 case "Vindicator Assassin Mask":
