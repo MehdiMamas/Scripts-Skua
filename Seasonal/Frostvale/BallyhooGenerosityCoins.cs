c48114c25d2c730e2d8454ef5749f34123106efd
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-12-21 16:20:25 +0100

    Add Ballyhoo's Generosity Coins script for seasonal quest completion

---
+++ Seasonal/Frostvale/BallyhooGenerosityCoins.cs
@@ -0,0 +1,36 @@
+/*
+name: BallyCool's Generosity Coins
+description: This will complete the quest that gets you 2 free Generosity Coins.
+tags: ballyhoo,bally-hoo,ballycool,generosity,gc,generositycoin,generosity coin,coins, seasonal, frostvale
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Seasonal\Frostvale\ChillysParticipation.cs
+using Skua.Core.Interfaces;
+
+public class BallyhooGC
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private ChillysQuest Chillys => new();
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        GetCoins();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetCoins()
+    {
+        if (Core.isCompletedBefore(10016) || !Core.isSeasonalMapActive("frostvale"))
+            return;
+
+        Chillys.ChillysParticipation();
+        Core.AddDrop("Generosity Coin");
+        Core.EnsureAccept(10016);
+        Core.HuntMonsterMapID("battleontown", 1, "Stolen Bag of Coins");
+        Core.EnsureComplete(10016);
+    }
+}
