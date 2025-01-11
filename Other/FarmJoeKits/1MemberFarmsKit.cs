b826e9efe032bdd51e7ef2d500e8a295b38145a5
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-01-11 16:08:48 +0100

    Add DragonBlade inclusion to various scripts

--- Other/FarmJoeKits/1MemberFarmsKit.cs
+++ Other/FarmJoeKits/1MemberFarmsKit.cs
@@ -107,6 +107,7 @@ tags: member, kit, expire, legend
 //cs_include Scripts/Nation/Various/TheLeeryContract[Member].cs
 //cs_include Scripts/Nation/Various/JuggernautItems.cs
 //cs_include Scripts/Nation/MergeShops/NulgathDiamondMerge.cs
+//cs_include Scripts/Nation\Various\DragonBlade[mem].cs
 //cs_include Scripts/Nation/Various/TarosManslayer.cs
 //cs_include Scripts/Nation/Various/PurifiedClaymoreOfDestiny.cs
 //cs_include Scripts/Nation/Various/VoidSpartan.cs
