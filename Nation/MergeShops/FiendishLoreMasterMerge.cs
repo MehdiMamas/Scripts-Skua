edc39de6e6550b7cfa84d2219b599df70abd75fc
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-02-05 15:57:57 +0100

    Fix include path for Tercessuinotlim.cs in merge shop scripts

--- Nation/MergeShops/FiendishLoreMasterMerge.cs
+++ Nation/MergeShops/FiendishLoreMasterMerge.cs
@@ -7,7 +7,7 @@ tags: fiendish, loremaster, merge, tercessuinotlim, archfiend, warrior, armet, c
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/CoreAdvanced.cs
-//cs_include Scripts/Story\Nation\Tercessuinotlim.cs
+//cs_include Scripts/Story/Nation/Tercessuinotlim.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Options;
