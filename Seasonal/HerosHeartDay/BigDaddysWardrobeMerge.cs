833f971f35db6f26b5e932d43b3465038f505dc6
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-02-20 00:32:43 +0100

    Fix path separator in TunnelOfLove.cs include statement

--- Seasonal/HerosHeartDay/BigDaddysWardrobeMerge.cs
+++ Seasonal/HerosHeartDay/BigDaddysWardrobeMerge.cs
@@ -7,7 +7,7 @@ tags: big, daddys, wardrobe, merge, tunneloflove, lovely, moonlighters, moonligh
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/Seasonal\HerosHeartDay\TunnelOfLove.cs
+//cs_include Scripts/Seasonal/HerosHeartDay/TunnelOfLove.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Options;
