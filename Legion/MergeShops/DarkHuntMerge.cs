efdee787f96c1a34c628eedbf13013a25169f31c
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-03-03 23:33:42 +0100

    Fix include path for CoreLegion in DarkHuntMerge script

--- Legion/MergeShops/DarkHuntMerge.cs
+++ Legion/MergeShops/DarkHuntMerge.cs
@@ -7,7 +7,7 @@ tags: dark, hunt, merge, darkfortress, bone, crusher, knight, skull, centurion, 
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/Legion\CoreLegion.cs
+//cs_include Scripts/Legion/CoreLegion.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Options;
