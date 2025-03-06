48fa86e15720ba52f350049c8151e9499cb5034c
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-03-06 23:45:49 +0100

    update description for Paragon Greatsword BattleGear script to specify quest ID

--- Seasonal/StaffBirthdays/DageTheEvil/ParagonGreatsword.cs
+++ Seasonal/StaffBirthdays/DageTheEvil/ParagonGreatsword.cs
@@ -1,6 +1,6 @@
 /*
 name: Paragon Greatsword BattleGear
-description: This script will complete the "Bonus Quest: Soul Food" for Paragon Greatsword BattleGear.
+description: This script will complete the "Soul Food" [10104] quest for Paragon Greatsword BattleGear.
 tags: nation, dage, birthday, seasonal, soul food, bonus quest, paragon,greatsword,battlegear
 */
 //cs_include Scripts/CoreBots.cs
