b32796ebc8b6aab917d8848951e5fd62d699edc9
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-03-08 13:26:16 +0100

    add Librarys Lost and Found Merge script for item farming in legionlibrary

---
+++ Legion/MergeShops/LibrarysLostandFoundMerge.cs
@@ -0,0 +1,139 @@
+/*
+name: Librarys Lost and Found Merge
+description: This bot will farm the items belonging to the selected mode for the Librarys Lost and Found Merge [2563] in /legionlibrary
+tags: librarys, lost, and, found, merge, legionlibrary, argus, panoptes, phlegethon, magma, lava, legion, loremaster, tome, lorekeeper, veil
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class LibrarysLostandFoundMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+
+    public bool DontPreconfigure = true;
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Argus' Iris", "Underworld Linen", "River Glowstone" });
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("legionlibrary", 2563, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Argus' Iris":
+                    Iris(quant);
+                    break;
+
+                case "Underworld Linen":
+                    Linen(quant);
+                    break;
+
+                case "River Glowstone":
+                    Glowstone(quant);
+                    break;
+
+            }
+        }
+    }
+
+    public void Iris(int quant = 1000)
+    {
+        Core.FarmingLogger("Argus' Iris", quant);
+        while (!Bot.ShouldExit && !Core.CheckInventory("Argus' Iris", quant))
+        {
+            Core.HuntMonsterQuest(10097, ("legionlibrary", "Argus Panoptes", ClassType.Solo));
+            Bot.Wait.ForPickup("Argus' Iris");
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void Linen(int quant = 1000)
+    {
+        Core.FarmingLogger("Underworld Linen", quant);
+        if (!Core.isCompletedBefore(10098))
+        {
+            Core.HuntMonsterQuest(10098,
+                ("dagefortress", "Scorned Knight", ClassType.Farm));
+        }
+        while (!Bot.ShouldExit && !Core.CheckInventory("Underworld Linen", quant))
+        {
+            Core.HuntMonsterQuest(10099,
+                            ("judgement", "Minos", ClassType.Solo),
+                            ("judgement", "Rhadamanthys", ClassType.Solo),
+                            ("judgement", "Aeacus", ClassType.Solo));
+            Bot.Wait.ForPickup("Underworld Linen");
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void Glowstone(int quant = 1000)
+    {
+        Core.FarmingLogger("River Glowstone", quant);
+        if (!Core.isCompletedBefore(10098))
+            Glowstone(2);
+        if (!Core.isCompletedBefore(10010))
+        {
+            Core.HuntMonsterQuest(10010,
+            ("legionlibrary", "Cerberus Pup", ClassType.Farm));
+        }
+        while (!Bot.ShouldExit && !Core.CheckInventory("River Glowstone", quant))
+        {
+            Core.HuntMonsterQuest(10101,
+                            ("styx", "Styx Hydra", ClassType.Solo));
+            Bot.Wait.ForPickup("River Glowstone");
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("92402", "Argus Panoptes Gauntlets", "Mode: [select] only\nShould the bot buy \"Argus Panoptes Gauntlets\" ?", false),
+        new Option<bool>("92401", "Argus Panoptes Gauntlet", "Mode: [select] only\nShould the bot buy \"Argus Panoptes Gauntlet\" ?", false),
+        new Option<bool>("92400", "Phlegethon Magma", "Mode: [select] only\nShould the bot buy \"Phlegethon Magma\" ?", false),
+        new Option<bool>("92399", "Phlegethon Lava", "Mode: [select] only\nShould the bot buy \"Phlegethon Lava\" ?", false),
+        new Option<bool>("92305", "Legion Loremaster Tome", "Mode: [select] only\nShould the bot buy \"Legion Loremaster Tome\" ?", false),
+        new Option<bool>("92304", "Legion Lorekeeper Veil", "Mode: [select] only\nShould the bot buy \"Legion Lorekeeper Veil\" ?", false),
+        new Option<bool>("92303", "Legion Loremaster Veil", "Mode: [select] only\nShould the bot buy \"Legion Loremaster Veil\" ?", false),
+        new Option<bool>("92302", "Legion Loremaster", "Mode: [select] only\nShould the bot buy \"Legion Loremaster\" ?", false),
+    };
+}
