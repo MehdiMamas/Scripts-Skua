39bbd582c082e004e8075280c0231a8a8d929501
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-03-16 15:32:01 +0100

    Create StrangersMerge.cs

---
+++ Legion/MergeShops/StrangersMerge.cs
@@ -0,0 +1,83 @@
+/*
+name: Strangers Merge
+description: This bot will farm the items belonging to the selected mode for the Strangers Merge [464] in /evilwardage
+tags: strangers, merge, evilwardage, skullborne, stranger, house, guard
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Legion/CoreLegion.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class StrangersMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+    private CoreLegion Legion = new();
+
+    public bool DontPreconfigure = true;
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Dage's Favor", "Dage's Approval" });
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("evilwardage", 464, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Dage's Favor":
+                    Legion.ApprovalAndFavor(0, quant);
+                    break;
+
+                case "Dage's Approval":
+                    Legion.ApprovalAndFavor(quant, 0);
+                    break;
+
+            }
+        }
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("29582", "SkullBorne Axe", "Mode: [select] only\nShould the bot buy \"SkullBorne Axe\" ?", false),
+        new Option<bool>("92252", "The Stranger House Guard", "Mode: [select] only\nShould the bot buy \"The Stranger House Guard\" ?", false),
+    };
+}
