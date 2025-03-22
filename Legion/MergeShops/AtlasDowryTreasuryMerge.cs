afec0afbadc441eb26212f448489d71be3d6811c
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-03-22 16:30:30 +0100

    Add Atlas Dowry Treasury Merge and Atlas Kingdom Story scripts

---
+++ Legion/MergeShops/AtlasDowryTreasuryMerge.cs
@@ -0,0 +1,140 @@
+/*
+name: Atlas Dowry Treasury Merge
+description: This bot will farm the items belonging to the selected mode for the Atlas Dowry Treasury Merge [2565] in /atlaskingdom
+tags: atlas, dowry, treasury, merge, atlaskingdom, spell, book, light, mage, morph, wedding, attire, bell, rose, bush, arch, matrimony, pact, dias, souledge, armblades, armblade, shuriken, shadow, soulblades, soulblade, sheath, sheathed, master, shadows, veil
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/Legion/SevenCircles(War).cs
+//cs_include Scripts/Story\Legion\AtlasPromenade.cs
+//cs_include Scripts/Story\Legion\AtlasKingdom.cs
+//cs_include Scripts/Legion\CoreLegion.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class AtlasDowryTreasuryMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+    private AtlasKingdom AK = new();
+    private CoreLegion Legion = new();
+
+    public bool DontPreconfigure = true;
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Atlas Lion Pelt", "Atlas Crest", "Coelho's Tome", "Blue Dye", "Legion Token" });
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+        AK.Storyline();
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("atlaskingdom", 2565, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Atlas Lion Pelt":
+                    Core.FarmingLogger(req.Name, quant);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonsterQuest(10126,
+                        ("atlaskingdom", "Atlas Leo", ClassType.Solo),
+                        ("atlaskingdom", "Atlas Elite", ClassType.Solo),
+                        ("atlaskingdom", "Executioner Ladon", ClassType.Solo));
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    break;
+
+                case "Atlas Crest":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.HuntMonster("atlaskingdom", "Atlas Light Magus", req.Name, quant, false, false);
+                    break;
+
+                case "Coelho's Tome":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Solo);
+                    Core.HuntMonster("atlaskingdom", "Coelho", req.Name, quant, false, false);
+                    break;
+
+                case "Blue Dye":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.RegisterQuests(5898);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        //QuarterMaster’s Supplies 5898
+                        Core.HuntMonster("ashfallcamp", "Lava Dragoblin", "Supply Chest", 8, log: false);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Legion Token":
+                    Legion.FarmLegionToken(quant);
+                    break;
+
+            }
+        }
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("92539", "Atlas Spell Book", "Mode: [select] only\nShould the bot buy \"Atlas Spell Book\" ?", false),
+        new Option<bool>("92538", "Atlas Light Mage Visage", "Mode: [select] only\nShould the bot buy \"Atlas Light Mage Visage\" ?", false),
+        new Option<bool>("92537", "Atlas Light Mage Morph", "Mode: [select] only\nShould the bot buy \"Atlas Light Mage Morph\" ?", false),
+        new Option<bool>("92536", "Atlas Light Mage", "Mode: [select] only\nShould the bot buy \"Atlas Light Mage\" ?", false),
+        new Option<bool>("92493", "Atlas Wedding Attire", "Mode: [select] only\nShould the bot buy \"Atlas Wedding Attire\" ?", false),
+        new Option<bool>("92492", "Atlas Wedding Bell", "Mode: [select] only\nShould the bot buy \"Atlas Wedding Bell\" ?", false),
+        new Option<bool>("92491", "Atlas Rose Bush", "Mode: [select] only\nShould the bot buy \"Atlas Rose Bush\" ?", false),
+        new Option<bool>("92490", "Atlas Wedding Arch", "Mode: [select] only\nShould the bot buy \"Atlas Wedding Arch\" ?", false),
+        new Option<bool>("92489", "Atlas Matrimony Pact", "Mode: [select] only\nShould the bot buy \"Atlas Matrimony Pact\" ?", false),
+        new Option<bool>("92488", "Atlas Wedding Dias", "Mode: [select] only\nShould the bot buy \"Atlas Wedding Dias\" ?", false),
+        new Option<bool>("92460", "Souledge Armblades", "Mode: [select] only\nShould the bot buy \"Souledge Armblades\" ?", false),
+        new Option<bool>("92459", "Souledge Armblade", "Mode: [select] only\nShould the bot buy \"Souledge Armblade\" ?", false),
+        new Option<bool>("92458", "Dual Souledge Shuriken", "Mode: [select] only\nShould the bot buy \"Dual Souledge Shuriken\" ?", false),
+        new Option<bool>("92457", "Souledge Shuriken", "Mode: [select] only\nShould the bot buy \"Souledge Shuriken\" ?", false),
+        new Option<bool>("92456", "Shadow Soulblades", "Mode: [select] only\nShould the bot buy \"Shadow Soulblades\" ?", false),
+        new Option<bool>("92455", "Shadow Soulblade", "Mode: [select] only\nShould the bot buy \"Shadow Soulblade\" ?", false),
+        new Option<bool>("92454", "Soulblade Sheath", "Mode: [select] only\nShould the bot buy \"Soulblade Sheath\" ?", false),
+        new Option<bool>("92453", "Sheathed Soulblade", "Mode: [select] only\nShould the bot buy \"Sheathed Soulblade\" ?", false),
+        new Option<bool>("92452", "Soulblade Master Mask", "Mode: [select] only\nShould the bot buy \"Soulblade Master Mask\" ?", false),
+        new Option<bool>("92451", "Soulblade Master of Shadows", "Mode: [select] only\nShould the bot buy \"Soulblade Master of Shadows\" ?", false),
+        new Option<bool>("92775", "Atlas Wedding Veil", "Mode: [select] only\nShould the bot buy \"Atlas Wedding Veil\" ?", false),
+    };
+}
---
+++ Story/Legion/AtlasKingdom.cs
@@ -0,0 +1,118 @@
+/*
+name: Atlas Kingdom Story
+description: This will finish General Slaine's questline in /atlaskingdom.
+tags: story, quest, legion,dage,atlas,kingdom,atlas-kingdom,general slaine,slaine,atlaskingdom
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/Legion/SevenCircles(War).cs
+//cs_include Scripts/Story\Legion\AtlasPromenade.cs
+using Skua.Core.Interfaces;
+
+public class AtlasKingdom
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    private AtlasPromenade AP = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Storyline();
+
+        Core.SetOptions(false);
+    }
+
+
+    public void Storyline()
+    {
+        if (Core.isCompletedBefore(10125))
+            return;
+
+        AP.Storyline();
+        Story.PreLoad(this);
+
+        #region Useable Monsters
+        string[] UseableMonsters = new[]
+        {
+    "Atlas Knight", // UseableMonsters[0],
+	"Atlas Light Magus", // UseableMonsters[1],
+	"Coelho", // UseableMonsters[2],
+	"Atlas Leo", // UseableMonsters[3],
+	"Atlas Elite", // UseableMonsters[4],
+	"Executioner Ladon", // UseableMonsters[5]
+};
+        #endregion Useable Monsters
+
+        // 10116 | Knight's Honor
+        Story.KillQuest(10116, "atlaskingdom", UseableMonsters[0]);
+        Story.MapItemQuest(10116, "atlaskingdom", 14252);
+
+
+        // 10117 | Drawn Light
+        Story.MapItemQuest(10117, "atlaskingdom", new[] { 14253, 14254, 14255 });
+
+
+
+        // 10118 | Sterile Life
+        if (!Story.QuestProgression(10118))
+        {
+            Core.HuntMonsterQuest(10118,
+                ("atlaskingdom", UseableMonsters[1], ClassType.Farm));
+        }
+
+
+        // 10119 | Lost Generations
+        Story.MapItemQuest(10119, "atlaskingdom", new[] { 14256, 14257 });
+
+
+
+        // 10120 | The Snitch
+        if (!Story.QuestProgression(10120))
+        {
+            Core.HuntMonsterQuest(10120,
+                ("atlaskingdom", UseableMonsters[2], ClassType.Solo));
+        }
+
+
+        // 10121 | Atlantides' Eve
+        Story.MapItemQuest(10121, "atlaskingdom", new[] { 14258, 14259, 14260 });
+
+
+        // 10122 | Lowly Form
+        if (!Story.QuestProgression(10122))
+        {
+            Core.HuntMonsterQuest(10122,
+                ("atlaskingdom", UseableMonsters[3], ClassType.Solo));
+        }
+
+
+        // 10123 | Enduring Sacrilege
+        if (!Story.QuestProgression(10123))
+        {
+            Core.HuntMonsterQuest(10123,
+                ("atlaskingdom", UseableMonsters[4], ClassType.Solo));
+        }
+
+
+        // 10124 | Blood of the Hesperides
+        if (!Story.QuestProgression(10124))
+        {
+            Core.HuntMonsterQuest(10124,
+                ("atlaskingdom", UseableMonsters[3], ClassType.Solo),
+                ("atlaskingdom", UseableMonsters[4], ClassType.Solo));
+        }
+
+
+        // 10125 | Prota Sparasso
+        if (!Story.QuestProgression(10125))
+        {
+            Core.HuntMonsterQuest(10125,
+                ("atlaskingdom", UseableMonsters[5], ClassType.Solo));
+        }
+
+
+    }
+}
