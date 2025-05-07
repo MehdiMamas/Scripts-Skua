f5472a131e4e4c57a73992ca8a22bc5d75d7f192
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-05-07 22:52:43 +0200

    Add Mawa's Quests Rewards script to farm seasonal rewards

---
+++ Seasonal/MayThe4th/MawaQuestRewards.cs
@@ -0,0 +1,61 @@
+/*
+name: Mawa's Quests Rewards
+description: This will farm the rewards from Mawa's Quests in /eventhub.
+tags: mawa, seasonal, may the 4th,quest reward,its a trap, it's a trap,droid for sale,get me parts, trobba fett house guard
+*/
+//cs_include Scripts/CoreBots.cs
+// //cs_include Scripts/CoreFarms.cs
+using Skua.Core.Interfaces;
+
+public class MawaQuestRewards
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetRewards();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetRewards()
+    {
+        DroidForSale();
+        GetMeParts();
+    }
+
+    public void DroidForSale()
+    {
+        if (Core.CheckInventory(Core.QuestRewards(9745)) || !Core.isSeasonalMapActive("murdermoon"))
+            return;
+
+        Core.AddDrop(Core.QuestRewards(9745));
+
+        //Droid For Sale (9745)
+        string[] reqs = Core.QuestRequirements<string>(9745);
+        Farm.Voucher("Gold Voucher 25k", 1);
+        Core.HuntMonster("murdermoon", "Tempest Soldier", reqs[1], isTemp: false);
+        Core.HuntMonster("twigguhunt", "Infantry Droid", reqs[2], isTemp: false);
+        Core.ChainComplete(9745);
+        Bot.Wait.ForDrop(Core.QuestRewards(9745)[0]);
+    }
+    public void GetMeParts()
+    {
+        if (Core.CheckInventory(Core.QuestRewards(10241)) || !Core.isSeasonalMapActive("murdermoon"))
+            return;
+
+        Core.AddDrop(Core.QuestRewards(10241));
+
+        //Get Me Parts (10241)
+        Core.HuntMonsterQuest(10241,
+        ("twigguhunt", "Infantry Droid", ClassType.Farm),
+        ("murdermoon", "Tempest Soldier", ClassType.Farm),
+        ("murdermoon", "Fifth Sepulchure", ClassType.Solo),
+        ("eventhub", "Moon Guard", ClassType.Solo));
+        Bot.Wait.ForDrop(Core.QuestRewards(10241)[0]);
+    }
+}
