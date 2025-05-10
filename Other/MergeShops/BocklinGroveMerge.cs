b32f9a3c0666070037ce3a25a0f6373a3aaaadd7
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-05-10 15:44:51 +0200

    Add Bocklin Grove Merge bot script for item farming

---
+++ Other/MergeShops/BocklinGroveMerge.cs
@@ -0,0 +1,120 @@
+/*
+name: Bocklin Grove Merge
+description: This bot will farm the items belonging to the selected mode for the Bocklin Grove Merge [2576] in /bocklingrove
+tags: bocklin, grove, merge, bocklingrove, champion, lynaria, chiral, valley, knight, duke, noble, antlered, golden, anjou, regal, visor, dukes, vaughns, sash, aquitaine
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/Lynaria/CoreLynaria.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class BocklinGroveMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+    private CoreLynaria Lyn = new();
+
+    public bool DontPreconfigure = true;
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Enthralling Gem Shard", "Victorious' Golden Scale", "Vaughn Crest", "Golden Anjou Helm", "Vaughn's Crimson Sash", "Sheathed Aquitaine" });
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+        Lyn.BocklinGrove();
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("bocklingrove", 2576, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Enthralling Gem Shard":
+                    Core.FarmingLogger(req.Name, quant);
+                    if (Core.IsMember)
+                        Core.RegisterQuests(10242);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonsterQuest(10240,
+                        ("bocklingrove", "Elder Necromancer", ClassType.Solo),
+                        ("bocklingrove", "Undead Garde", ClassType.Farm),
+                        ("bocklingrove", "Garde Wraith", ClassType.Farm));
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Victorious' Golden Scale":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Solo);
+                    Core.HuntMonster("darkplane", "Victorious", req.Name, quant, req.Temp, false);
+                    break;
+
+                case "Vaughn Crest":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.HuntMonster("bocklingrove", "Garde Wraith", req.Name, quant, req.Temp, false);
+                    break;
+
+                case "Golden Anjou Helm":
+                case "Vaughn's Crimson Sash":
+                case "Sheathed Aquitaine":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Solo);
+                    Core.HuntMonster("bocklingrove", "Elder Necromancer", req.Name, quant, req.Temp, false);
+                    break;
+            }
+        }
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("93590", "Champion Lynaria Armor", "Mode: [select] only\nShould the bot buy \"Champion Lynaria Armor\" ?", false),
+        new Option<bool>("93613", "Chiral Valley Knight", "Mode: [select] only\nShould the bot buy \"Chiral Valley Knight\" ?", false),
+        new Option<bool>("93614", "Chiral Valley Duke", "Mode: [select] only\nShould the bot buy \"Chiral Valley Duke\" ?", false),
+        new Option<bool>("93615", "Chiral Valley Noble", "Mode: [select] only\nShould the bot buy \"Chiral Valley Noble\" ?", false),
+        new Option<bool>("93616", "Antlered Golden Anjou Helm", "Mode: [select] only\nShould the bot buy \"Antlered Golden Anjou Helm\" ?", false),
+        new Option<bool>("93617", "Regal Golden Anjou Helm", "Mode: [select] only\nShould the bot buy \"Regal Golden Anjou Helm\" ?", false),
+        new Option<bool>("93619", "Antlered Golden Anjou Visor", "Mode: [select] only\nShould the bot buy \"Antlered Golden Anjou Visor\" ?", false),
+        new Option<bool>("93620", "Golden Anjou Visor", "Mode: [select] only\nShould the bot buy \"Golden Anjou Visor\" ?", false),
+        new Option<bool>("93621", "Anjou Duke's Helm", "Mode: [select] only\nShould the bot buy \"Anjou Duke's Helm\" ?", false),
+        new Option<bool>("93624", "Vaughn's Sash and Aquitaine", "Mode: [select] only\nShould the bot buy \"Vaughn's Sash and Aquitaine\" ?", false),
+        new Option<bool>("93625", "Aquitaine", "Mode: [select] only\nShould the bot buy \"Aquitaine\" ?", false),
+        new Option<bool>("93626", "Dual Aquitaine", "Mode: [select] only\nShould the bot buy \"Dual Aquitaine\" ?", false),
+    };
+}
