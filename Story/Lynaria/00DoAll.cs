64181e4e15dfc6e58a6941eb44e198c1cc9097b5
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-05-18 19:48:35 +0200

    Update tags in Lynaria's Saga (Do All) for consistency and clarity

--- Story/Lynaria/00DoAll.cs
+++ Story/Lynaria/00DoAll.cs
@@ -1,7 +1,7 @@
 /*
 name: Lynaria's Saga (Do All)
 description: This will finish the Lynaria's Saga storyline in /bocklingrove.
-tags: story, quest, saga, doall, lynaria, complete, all, queen lynaria, bocklingrove, bocklin grove,victoria,victoria alteon
+tags: story, quest, saga, doall, lynaria, complete, all, queen lynaria, bocklingrove, bocklin grove, bocklincastle, bocklin castle,victoria,victoria alteon
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreStory.cs
