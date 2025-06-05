161d4cec4a2ad80e1f19fc49cad50489440160bc
Tato <47118168+wtffidy@users.noreply.github.com>
2025-06-05 12:41:47 -0400

    Update 1GetAllRanks.cs

--- Farm/REP/1GetAllRanks.cs
+++ Farm/REP/1GetAllRanks.cs
@@ -10,6 +10,7 @@ tags: all reps, reputation, rank, all ranks, farm, rep, reps
 //cs_include Scripts/Story/LordsofChaos/Core13LoC.cs    
 //cs_include Scripts/Story/ThroneofDarkness/CoreToD.cs
 //cs_include Scripts/Story/Glacera.cs
+//cs_include Scripts/Story/QueenofMonsters/Extra/BrightOak.cs
 
 using Skua.Core.Interfaces;
 public class GetAllRanks
@@ -22,6 +23,7 @@ public class GetAllRanks
     public CoreToD TOD = new();
     public Core13LoC LOC => new();
     public GlaceraStory Glac => new();
+    public BrightOak BrightOak = new();
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -42,6 +44,8 @@ public class GetAllRanks
         Core.Logger("Doing Required Stories for the reps, let tato know if another is required.");
         TOD.CompleteToD();
         LOC.Complete13LOC();
+        BrightOak.doall(true);
+        Core.Logger("when doing the `Glacera` storyline, you may have to restart it in the middle of the quests due to ae's bullshit how it keeps another quest locked even though the preivosu is completed till you relog");
         Glac.DoAll();
         // Commented out do to PvP (with farm class or deaths) is still broke appearnly :thumbsup:
         // TOD.DeathPitPVP();
