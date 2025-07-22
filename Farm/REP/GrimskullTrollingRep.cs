ae84018e1fa318f15674ea5272605720803e2cd0
Tato <47118168+wtffidy@users.noreply.github.com>
2025-07-22 10:58:54 -0400

    Create GrimskullTrollingRep.cs

---
+++ Farm/REP/GrimskullTrollingRep.cs
@@ -0,0 +1,169 @@
+/*
+name: GrimskullTrollingRep
+description: Goes through the grimgaol dungeon multiple times for reputation
+tags: Grimskull trolling, Grimskull, Rep, Reputation, Grimgaol, Lich, Verata's Necronomicon
+*/
+
+#region includes
+//cs_include Scripts/Chaos/DrakathsArmor.cs
+//cs_include Scripts/Chaos/EternalDrakathSet.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Dailies/LordOfOrder.cs
+//cs_include Scripts/Darkon/CoreDarkon.cs
+//cs_include Scripts/Darkon/Various/PrinceDarkonsPoleaxePreReqs.cs
+//cs_include Scripts/Enhancement/UnlockForgeEnhancements.cs
+//cs_include Scripts/Evil/ADK.cs
+//cs_include Scripts/Evil/NSoD/CoreNSOD.cs
+//cs_include Scripts/Evil/SDKA/CoreSDKA.cs
+//cs_include Scripts/Evil/SepulchuresOriginalHelm.cs
+//cs_include Scripts/Good/ArchPaladin.cs
+//cs_include Scripts/Good/BLoD/CoreBLOD.cs
+//cs_include Scripts/Good/GearOfAwe/ArmorOfAwe.cs
+//cs_include Scripts/Good/GearOfAwe/Awescended.cs
+//cs_include Scripts/Good/GearOfAwe/CoreAwe.cs
+//cs_include Scripts/Good/GearOfAwe/HelmOfAwe.cs
+//cs_include Scripts/Good/Paladin.cs
+//cs_include Scripts/Good/SilverExaltedPaladin.cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+//cs_include Scripts/Hollowborn/TradingandStuff(single).cs
+//cs_include Scripts/Legion/CoreLegion.cs
+//cs_include Scripts/Legion/HeadOfTheLegionBeast.cs
+//cs_include Scripts/Legion/SwordMaster.cs
+//cs_include Scripts/Legion/YamiNoRonin/CoreYnR.cs
+//cs_include Scripts/Nation/AFDL/NulgathDemandsWork.cs
+//cs_include Scripts/Nation/AFDL/WillpowerExtraction.cs
+//cs_include Scripts/Nation/AssistingCragAndBamboozle[Mem].cs
+//cs_include Scripts/Nation/CoreNation.cs
+//cs_include Scripts/Nation/EmpoweringItems.cs
+//cs_include Scripts/Nation/MergeShops/DilligasMerge.cs
+//cs_include Scripts/Nation/MergeShops/DirtlickersMerge.cs
+//cs_include Scripts/Nation/MergeShops/NationMerge.cs
+//cs_include Scripts/Nation/MergeShops/NulgathDiamondMerge.cs
+//cs_include Scripts/Nation/MergeShops/VoidChasmMerge.cs
+//cs_include Scripts/Nation/MergeShops/VoidRefugeMerge.cs
+//cs_include Scripts/Nation/NationLoyaltyRewarded.cs
+//cs_include Scripts/Nation/VHL/CoreVHL.cs
+//cs_include Scripts/Nation/Various/ArchfiendDeathLord.cs
+//cs_include Scripts/Nation/Various/DragonBlade[mem].cs
+//cs_include Scripts/Nation/Various/GoldenHanzoVoid.cs
+//cs_include Scripts/Nation/Various/JuggernautItems.cs
+//cs_include Scripts/Nation/Various/PrimeFiendShard.cs
+//cs_include Scripts/Nation/Various/PurifiedClaymoreOfDestiny.cs
+//cs_include Scripts/Nation/Various/SwirlingTheAbyss.cs
+//cs_include Scripts/Nation/Various/TarosManslayer.cs
+//cs_include Scripts/Nation/Various/TheLeeryContract[Member].cs
+//cs_include Scripts/Nation/Various/VoidPaladin.cs
+//cs_include Scripts/Nation/Various/VoidSpartan.cs
+//cs_include Scripts/Other/Armor/FireChampionsArmor.cs
+//cs_include Scripts/Other/Armor/MalgorsArmorSet.cs
+//cs_include Scripts/Other/Classes/DragonOfTime.cs
+//cs_include Scripts/Other/Classes/DragonslayerGeneral.cs
+//cs_include Scripts/Other/Classes/Necromancer.cs
+//cs_include Scripts/Other/MergeShops/InfernalArenaMerge.cs
+//cs_include Scripts/Other/MysteriousEgg.cs
+//cs_include Scripts/Other/ShadowDragonDefender.cs
+//cs_include Scripts/Other/WarFuryEmblem.cs
+//cs_include Scripts/Other/Weapons/FortitudeAndHubris.cs
+//cs_include Scripts/Other/Weapons/GoldenBladeOfFate.cs
+//cs_include Scripts/Other/Weapons/PinkBladeofDestruction.cs
+//cs_include Scripts/Other/Weapons/ShadowReaperOfDoom.cs
+//cs_include Scripts/Other/Weapons/VoidAvengerScythe.cs
+//cs_include Scripts/Other/Weapons/WrathofNulgath.cs
+//cs_include Scripts/ShadowsOfWar/CoreSoWMats.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/DeadLinesMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/ManaCradleMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/ShadowflameFinaleMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/StreamwarMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/TimekeepMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/WorldsCoreMerge.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelve.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelveMerge.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleSiege.cs
+//cs_include Scripts/Story/7DeadlyDragons/Core7DD.cs
+//cs_include Scripts/Story/7DeadlyDragons/Extra/HatchTheEgg.cs
+//cs_include Scripts/Story/BattleUnder.cs
+//cs_include Scripts/Story/Borgars.cs
+//cs_include Scripts/Story/DjinnGate.cs
+//cs_include Scripts/Story/DoomVault.cs
+//cs_include Scripts/Story/DoomVaultB.cs
+//cs_include Scripts/Story/Doomwood/CoreDoomwood.cs
+//cs_include Scripts/Story/DragonFableOrigins.cs
+//cs_include Scripts/Story/ElegyofMadness(Darkon)/CoreAstravia.cs
+//cs_include Scripts/Story/J6Saga.cs
+//cs_include Scripts/Story/Lair.cs
+//cs_include Scripts/Story/Legion/DarkWarLegionandNation.cs
+//cs_include Scripts/Story/Legion/SevenCircles(War).cs
+//cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
+//cs_include Scripts/Story/Nation/Bamboozle.cs
+//cs_include Scripts/Story/Nation/CitadelRuins.cs
+//cs_include Scripts/Story/Nation/Fiendshard.cs
+//cs_include Scripts/Story/Nation/Originul.cs
+//cs_include Scripts/Story/Nation/VoidChasm.cs
+//cs_include Scripts/Story/Nation/VoidRefuge.cs
+//cs_include Scripts/Story/QueenofMonsters/CoreQoM.cs
+//cs_include Scripts/Story/QueenofMonsters/Extra/CelestialArena.cs
+//cs_include Scripts/Story/QueenofMonsters/Extra/InfernalArena.cs
+//cs_include Scripts/Story/SepulchureSaga/CoreSepulchure.cs
+//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
+//cs_include Scripts/Story/StarSinc.cs
+//cs_include Scripts/Story/Summer2015AdventureMap/CoreSummer.cs
+//cs_include Scripts/Story/ThirdSpell.cs
+//cs_include Scripts/Story/ThroneofDarkness/CoreToD.cs
+//cs_include Scripts/Story/TowerOfDoom.cs
+//cs_include Scripts/Story/XansLair.cs
+//cs_include Scripts/Story/Yokai.cs
+//cs_include Scripts/Prototypes/Grimgaol.cs
+#endregion includes
+
+using System.Threading.Tasks;
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Players;
+using Skua.Core.Options;
+using Newtonsoft.Json.Linq;
+using Skua.Core.Models.Monsters;
+using Skua.Core.Models.Items;
+using Skua.Core.Models.Auras;
+
+
+public class GrimskullTrollingRep
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public Grimgaol GrimGaolRun = new();
+    public static Grimgaol GGrimGaolRun = new();
+
+    public string OptionsStorage = GGrimGaolRun.OptionsStorage;
+    public List<IOption> Options = GGrimGaolRun.Options;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        DoGrimskullTrollingRep(10);
+    }
+
+    public void DoGrimskullTrollingRep(int rank = 10)
+    {
+        if (Farm.FactionRank("Grimskull Trolling") >= rank)
+        {
+            Core.Logger($"You already have rank {rank} Grimskull Trolling reputation.");
+            return;
+        }
+
+        Core.Logger($"Farming rank {rank}");
+
+        while (!Bot.ShouldExit && Farm.FactionRank("Grimskull Trolling") < rank)
+        {
+            Core.EnsureAccept(9467);
+            GrimGaolRun.DoGrimGaol();
+            Core.Join("whitemap");  
+            Core.Sleep(5000);
+            Core.EnsureComplete(9467);
+        }
+
+        Core.CancelRegisteredQuests();
+    }
+}
