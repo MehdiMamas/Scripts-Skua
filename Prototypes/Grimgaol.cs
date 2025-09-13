7dc99e94f9b33180240bd6a0dfcb6e811a0f49d5
Tato <47118168+wtffidy@users.noreply.github.com>
2025-09-13 12:41:38 -0400

    Update Grimgaol.cs

--- Prototypes/Grimgaol.cs
+++ Prototypes/Grimgaol.cs
@@ -1330,7 +1330,7 @@ public class Grimgaol
 
 
         int skillIndex = 0;
-        int[] skillList = { /*1, 2, 3, 4*/ 1, 2, 3, 1, 2, 4, 3, 2, 1, 3, 2, 4 };
+        int[] skillList = { 1, 2, 3, 4 /* 1, 2, 3, 1, 2, 4, 3, 2, 1, 3, 2, 4 */};
         // int[] skillList = { 1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 1, 2, 4, 3 };
 
 
